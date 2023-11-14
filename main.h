6c2af2eec332e6ee6c2f49dcedc355854a3b981e
Menna33 <mennalotfy33@gmail.com>
2023-11-14 05:01:12 +0200

    adding main.h

---
+++ main.h
@@ -0,0 +1,27 @@
+#ifndef MAIN_H
+#define MAIN_H
+#define BUFF_SIZE 1024
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * struct specifier - handle conversion specifiers
+ * @sp:..
+ * @f:..
+ */
+typedef struct specifier
+{
+	char *sp;
+	int (*f)(va_list);
+} sp_t;
+int _printf(const char *format, ...);
+int _printf_char(va_list args);
+int _printf_str(va_list args);
+int _printf_percent(va_list args __attribute__((unused)));
+int _putchar(char c);
+int printf_int(va_list args);
+int printf_dec(va_list args);
+int binary(va_list args);
+#endif
