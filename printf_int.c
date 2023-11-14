5789d5c306366935df905a498afa96a3170ba5bb
Menna33 <mennalotfy33@gmail.com>
2023-11-14 05:01:59 +0200

    adding printf_int.c

---
+++ printf_int.c
@@ -0,0 +1,66 @@
+#include "main.h"
+/**
+ * printf_int - prints integer
+ * @args: argument to print
+ * Return: number of characters printed
+ */
+int printf_int(va_list args)
+{
+
+int num, digit, base = 1, count = 0;
+num = va_arg(args, int);
+if (num < 0)
+{
+num = -1 * num;
+_putchar('-');
+count++;
+}
+digit = num / 10;
+while (num / base != 0)
+{
+base = base * 10;
+digit = digit / 10;
+}
+base = base / 10;
+while (base > 0)
+{
+_putchar((num / base)  +'0');
+num = num - ((num / base) *base);
+base = base / 10;
+count++;
+}
+return (count);
+}
+
+/**
+ * printf_dec - prints decimal number
+ * @args: argument to print
+ * Return: number of characters printed
+ */
+int printf_dec(va_list args)
+{
+
+int num, digit, base = 1, count = 0;
+num = va_arg(args, int);
+if (num < 0)
+{
+num = -1 * num;
+_putchar('-');
+count++;
+}
+digit = num / 10;
+while (num / base != 0)
+{
+base = base * 10;
+digit = digit / 10;
+}
+base = base / 10;
+while (base > 0)
+{
+_putchar((num / base)  +'0');
+num = num - ((num / base) *base);
+base = base / 10;
+count++;
+}
+return (count);
+}
